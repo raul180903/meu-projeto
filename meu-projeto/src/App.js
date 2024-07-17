4a935d4144cf90c63cf28d414f8a5c95efc8e463
Raul de Souza <rauldesouza.rds2@gmail.com>
2024-07-16 21:56:37 -0300

    atualizando o react js, aprendendo importa estilos do CSS

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-import DigaMeuNome from './componentes/DigaMeuNome';
-import Marcas from './componentes/Marcas'
 import Pessoa from './componentes/Pessoa'
-import TimesEuropeus from './componentes/TimesEuropeus';
+import Frase from './componentes/Frase';
 
 function App() {
 
@@ -11,23 +9,17 @@ function App() {
   const esporte = "Fórmula 1"
   const roupa = "Nike"
 
-
   return (
     <div className="App">
-      <DigaMeuNome nome="Raul" nomenamorada= "Ellen"/>
-      <Marcas carro1 = "Ferrari" carro2={carro2} esporte={esporte} roupa={roupa}/>
+      <Frase/>
       <Pessoa  
         nome = "Raul"
         idade = "20"
         profissão = "Programador Web"
         foto ="https://via.placeholder.com/150x150>"
       />    
-      <TimesEuropeus time1 = "Real Madrid" time2 = "Bayern" time3 = "Leverkusen" campeonato1="Brasileirão"/>
     </div>
-  )
-  
+  )  
 }
-
-
 
 export default App;
--- meu-projeto/src/componentes/Frase.js
+++ meu-projeto/src/componentes/Frase.js
@@ -1,10 +1,10 @@
-import Teste1 from './Teste1'
+import estilo from './Frase.module.css'
 
 function Frase () {
     return(
         <div>
-            <h2>Componente 2</h2>
-            <Teste1/>
+            <h2 className={estilo.estiloFrase}>Usando o CSS</h2>
+            <h2 className={estilo.estiloFrase2}>Testando CSS 2</h2>
         </div>
     )
 }
