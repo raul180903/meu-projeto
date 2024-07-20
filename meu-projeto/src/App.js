49c68ae9a592b7b6539660a59952f860c8f23871
Raul de Souza <rauldesouza.rds2@gmail.com>
2024-07-20 15:42:44 -0300

    aula de react js: aprendendo sobre Eventos

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,27 +1,19 @@
 import './App.css';
-import Pessoa from './componentes/Pessoa';
-import Frase from './componentes/Frase';
-import List from './componentes/List';
+import Evento from './componentes/Evento';
+
 
 function App() {
 
-  const nome = "Felipe"
-  const carro2 = "Mercedes"
-  const esporte = "Fórmula 1"
-  const roupa = "Nike"
-
   return (
-    <div className="App">
-      <Frase/>
-      <Pessoa  
-        nome = "Raul"
-        idade = "20"
-        profissão = "Programador Web"
-        foto ="https://via.placeholder.com/150x150>"
-      />  
-      <List/>  
+    <div>
+      <h1>Testando Eventos</h1>
+      /*praticar isso hoje*/ <br/>
+      /*continuar aula com eventos de formularios*/
+      <Evento numero = "1"/>
+      <Evento numero = "2"/>
     </div>
-  )  
+  )
+     
 }
 
 export default App;
