fe144d61ed91888d60cf15d0dc4f6bcc400a4352
Raul de Souza <rauldesouza.rds2@gmail.com>
2024-07-23 22:10:04 -0300

    hoje a aula foi de onSubmit

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import Evento from './componentes/Evento';
-
+import Form from './componentes/Form';
 
 function App() {
 
   return (
     <div>
       <h1>Testando Eventos</h1>
-      /*praticar isso hoje*/ <br/>
-      /*continuar aula com eventos de formularios*/
-      <Evento numero = "1"/>
-      <Evento numero = "2"/>
+      <Evento/>
+      <Form/>
     </div>
   )
      
